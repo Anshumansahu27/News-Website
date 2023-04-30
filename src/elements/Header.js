b0ecb7572569fa1c096b03f17f056fa473186ec0
Anshuman <1906678@kiit.ac.in>
2023-05-01 00:50:01 +0530

    6th commit

--- src/elements/Header.js
+++ src/elements/Header.js
@@ -5,20 +5,39 @@ import { useState } from 'react';
 
 class Header extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
 
   render() {
     return (<div>
-          <h2>
-            <Link to="/"><h1 className="head__text"><img className="logo" src={img1} alt="" />    KIIT NEWS</h1></Link>
-          </h2>
+            <div>
+            <h1 className="head__text"><Link to="/">
+              <img className="logo" src={img1} alt="" /></Link>    KIIT NEWS</h1>
+              
+          
 
-          <h2><input type="text" placeholder="search here"  /> <button class="button"><Link to="./{search}">search</Link></button>
+          <h2><input type="text" placeholder="search here" 
+          value={this.state.value} 
+          onChange={this.handleChange}/> 
+          <Link to="/{this.target.value}">
+            <button class="button">search</button>
+            </Link>
           </h2>
             <Link to="/">Headlines</Link>
             <Link to="/search/politics">Politics</Link>
             <Link to="/search/business">Business</Link>
             <Link to="/search/sports">Sports</Link>
             <Link to="/search/cryptos">Cryptos</Link>
+            </div>
             </div>
 
     );
